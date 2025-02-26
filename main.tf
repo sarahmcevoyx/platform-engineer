187f1949e590fff114d18c11f5b6c4408c12c6b8
sarahmcevoyx <sarahmcevoyx@outlook.com>
2025-02-26 20:56:03 +0000

    updating files

--- main.tf
+++ main.tf
@@ -26,17 +26,19 @@ resource "azurerm_subnet" "subnet" {
 
 # Setting up an Azure Key Vault with specific properties and access policies
 resource "azurerm_key_vault" "functions_kv" {
-  name                     = var.key_vault_name
-  resource_group_name      = azurerm_resource_group.functions_rg.name
-  location                 = var.resource_group_location
-  sku_name                 = var.key_vault_sku
-  tenant_id                = data.azurerm_client_config.current.tenant_id
-  purge_protection_enabled = var.enable_purge_protection
+  name                        = var.key_vault_name
+  resource_group_name         = azurerm_resource_group.functions_rg.name
+  location                    = var.resource_group_location
+  sku_name                    = var.key_vault_sku
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = var.soft_delete_retention_days
+  purge_protection_enabled    = var.enable_purge_protection
+  enabled_for_disk_encryption = var.disk_encryption
   access_policy {
-    tenant_id          = data.azurerm_client_config.current.tenant_id
-    object_id          = data.azurerm_client_config.current.object_id
-    secret_permissions = var.secret_permissions
-    key_permissions    = var.key_permissions
+    tenant_id                 = data.azurerm_client_config.current.tenant_id
+    object_id                 = data.azurerm_client_config.current.object_id
+    secret_permissions        = var.secret_permissions
+    key_permissions           = var.key_permissions
   }
   network_acls {
     default_action             = var.network_acls_default_action
@@ -47,11 +49,19 @@ resource "azurerm_key_vault" "functions_kv" {
 
 # Setting up a cryptographic key within an Azure Key Vault
 resource "azurerm_key_vault_key" "example_key" {
-  name         = var.key_name
-  key_vault_id = var.key_vault_id
-  key_type     = var.key_type
-  key_size     = var.key_size
-  key_opts     = var.key_opts
+  name                    = var.key_name
+  key_vault_id            = var.key_vault_id
+  key_type                = var.key_type
+  key_size                = var.key_size
+  key_opts                = var.key_opts
+  rotation_policy {
+    automatic {
+      time_before_expiry  = var.key_rotation_policy
+      time_after_creation = var.time_after_creation
+    }
+    expire_after          = var.key_expire_after
+    notify_before_expiry  = var.key_notify_before_expiry
+  }
 }
 
 # Defines an Azure Log Analytics Workspace
@@ -97,11 +107,11 @@ resource "azurerm_monitor_metric_alert" "kv_alert" {
   window_size         = var.kv_alert_window_size
   # evaluation_frequency = var.kv_alert_evaluation_frequency
   criteria {
-    metric_namespace = var.kv_alert_metric_namespace
-    metric_name      = var.kv_alert_metric_name
-    aggregation      = var.kv_alert_aggregation
-    operator         = var.kv_alert_operator
-    threshold        = var.kv_alert_threshold
+    metric_namespace  = var.kv_alert_metric_namespace
+    metric_name       = var.kv_alert_metric_name
+    aggregation       = var.kv_alert_aggregation
+    operator          = var.kv_alert_operator
+    threshold         = var.kv_alert_threshold
   }
 }
 
